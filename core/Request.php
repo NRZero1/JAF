e2ab6f187fadc6e583922492a645df74340c621d
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2022-12-17 10:28:24 +0700

    feat: add explicit return value and add method to get whether method is get or post

--- core/Request.php
+++ core/Request.php
@@ -4,7 +4,7 @@ namespace NRZero\JASF\Core;
 
 class Request
 {
-    public function getPath()
+    public function getPath(): string
     {
         $path = $_SERVER['REQUEST_URI'] ?? '/';
         $queryPosition = strpos($path, '?');
@@ -14,8 +14,18 @@ class Request
         return $path;
     }
 
-    public function getMethod()
+    public function getMethod(): string
     {
         return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+
+    public function isPost(): bool
+    {
+        return $this->getMethod() === 'get';
+    }
+
+    public function isGet(): bool
+    {
+        return $this->getMethod() === 'post';
     }
 }
