6585b3121e731902b67857f2e09cd4c374052cf8
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2022-12-17 10:27:08 +0700

    feat: add response, database, session, and view accessable from app

--- core/Application.php
+++ core/Application.php
@@ -6,15 +6,29 @@ class Application
 {
     public Router $router;
     public Request $request;
+    public Response $response;
+    public static string $ROOT_DIR;
+    public static Application $app;
+    public Database $db;
+    public Session $session;
+    public View $view;
 
-    public function __construct()
+    public function __construct(string $ROOT_DIR, array $config)
     {
-        $this->router = new Router();
+        self::$ROOT_DIR = $ROOT_DIR;
+        self::$app = $this;
         $this->request = new Request();
+        $this->response = new Response();
+        $this->session = new Session();
+        $this->view = new View();
+        $this->router = new Router($this->request, $this->response);
+        $this->db = new Database($config['db_host'], $config['db_name'], $config['db_username'], $config['db_pass']);
     }
 
     public function run()
     {
-        $this->router->resolve();
+        foreach ($this->router->resolve() as $value) {
+            echo $value;
+        }
     }
 }
