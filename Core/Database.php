4d41b72e097f20771e1038d6cb66f921b5fc932e
Daniel Ronaldo Pangestu <ronaldo.pangestu1@gmail.com>
2023-02-08 11:47:06 +0700

    feat: add CRUD and sortBy function

--- Core/Database.php
+++ Core/Database.php
@@ -2,18 +2,17 @@
 
 namespace NRZero\JAF\Core;
 
+use Attribute;
 use PDO;
 use PDOException;
 
 class Database
 {
     public \PDO $pdo;
-    private string $query;
     private string $tableName;
-    private string|array $columns;
-    private array $data;
-    private string $mode;
-    private array $joins;
+    private string $sortColumn;
+    private string $sortOrder;
+
     public function __construct(string $db_host, string $db_name, string $db_username, string $db_pass)
     {
         try {
@@ -24,43 +23,109 @@ class Database
             );
             $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            print_r("connected");
+            // print_r("connected");
         } catch (PDOException $error) {
             echo "Connection error: " . $error->getMessage();
         }
     }
 
-    public function prepareInsert(array $data)
-    {
-        $this->data = $data;
-        $this->mode = "INSERT";
-        return $this;
-    }
-
-    public function selectTable(string $tableName)
+    public function setTableName(string $tableName)
     {
         $this->tableName = $tableName;
         return $this;
     }
 
-    public function selectColumns(array $columns)
+    public function insert(array $attributes, mixed $postData)
     {
-        $this->columns = $columns;
-        return $this;
-    }
+        $preparedParams = array_map(fn($attr) => ":$attr", $attributes);
+        $tickedAttributes = array_map(fn($attr) => "`$attr`", $attributes);
+        $statement = $this->pdo->prepare(
+            "INSERT INTO $this->tableName (" . implode(",", $tickedAttributes) . ")
+            VALUES (" . implode(",", $preparedParams) . ")"
+        );
 
-    public function prepare()
-    {
-        switch ($this->mode) {
-            case "INSERT":
-                if (empty($this->columns)) {
-                    $this->query = "INSERT INTO {$this->tableName} VALUES (:";
-                }
+        for ($index = 0; $index < sizeof($attributes); $index++) {
+            if (is_array($postData)) {
+                $statement->bindValue($preparedParams[$index], $postData[$attributes[$index]]);
+            } else {
+                $statement->bindValue($preparedParams[$index], $postData);
+            }
         }
+        return $statement->execute();
     }
 
-    public function run()
+    public function update(array $attributes, mixed $postData, string $where, mixed $value)
     {
-        // run pdo query
+        $preparedParams = array_map(fn($attr) => ":$attr", $attributes);
+        $tickedAttributes = array_map(fn($attr) => "`$attr`", $attributes);
+        $query = "UPDATE $this->tableName SET ";
+
+        for ($index = 0; $index < sizeof($attributes); $index++) {
+            if ($index == sizeof($attributes) - 1) {
+                $query .= $tickedAttributes[$index] . "=" . "$preparedParams[$index] ";
+            } else {
+                $query .= $tickedAttributes[$index] . "=" . "$preparedParams[$index], ";
+            }
+        }
+
+        $query .= "WHERE $where = :$where";
+
+        $statement = $this->pdo->prepare($query);
+
+        for ($index = 0; $index < sizeof($attributes); $index++) {
+            if (is_array($postData)) {
+                $statement->bindValue($preparedParams[$index], $postData[$attributes[$index]]);
+            } else {
+                $statement->bindValue($preparedParams[$index], $postData);
+            }
+        }
+        $statement->bindValue(":$where", $value);
+        return $statement->execute();
+    }
+
+    public function delete(string $where, mixed $value)
+    {
+        $statement = $this->pdo->prepare(
+            "DELETE FROM $this->tableName
+            WHERE $where = :$where"
+        );
+        $statement->bindValue(":$where", $value);
+        return $statement->execute();
+    }
+
+    public function fetchOne(array $attributes, string $where, mixed $value)
+    {
+        $tickedAttributes = array_map(fn($attr) => "`$attr`", $attributes);
+        $statement = $this->pdo->prepare(
+            "SELECT " . implode(",", $tickedAttributes) .
+            " FROM $this->tableName WHERE $where = :$where"
+        );
+        $statement->bindValue(":$where", $value);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+
+    public function fetchAll(array $attributes = [])
+    {
+        if (empty($attributes)) {
+            $query = "SELECT * FROM $this->tableName";
+        } else {
+            $tickedAttributes = array_map(fn($attr) => "`$attr`", $attributes);
+            $query = "SELECT " . implode(",", $tickedAttributes) . " FROM $this->tableName";
+        }
+
+        if (!empty($this->sortColumn)) {
+            $query .= " ORDER BY '$this->sortColumn' $this->sortOrder";
+        }
+        $statement = $this->pdo->prepare($query);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+
+    public function sortBy(string $columnName, string $mode = 'ASC')
+    {
+        $this->sortColumn = $columnName;
+        $this->sortOrder = $mode;
+        return $this;
     }
 }
